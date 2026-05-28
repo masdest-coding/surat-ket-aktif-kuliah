3fd7d9b1ebaa6eceff33031a1d5ce973be37189c
masdest-coding <masdest1908@gmail.com>
2026-05-28 23:58:50 +0700

    ubah logo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 class PDF(FPDF):
     def header(self):
         # 1. Tambahkan Logo UMITRA (Pastikan file logo_umitra.png ada di folder)
-        self.image('Logo_umitra.png', 12, 12, 30) 
+        self.image('Logo_umitra.png', 10, 12, 30) 
         
         lebar_teks = self.w - 50
         
